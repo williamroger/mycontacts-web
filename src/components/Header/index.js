a51d0a03e4c557cbeb3a4afb8a485ed1b0f0248b
William Roger <williamroger.frontend@gmail.com>
2025-03-05 21:47:58 -0300

    adicionada nova tela Home e refatorado o componente Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import { Container, InputSearchContainer } from './styles';
+import { Container } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
@@ -6,10 +6,6 @@ function Header() {
   return (
     <Container>
       <img src={logo} alt="MyContacts" width="209" />
-
-      <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar contato..." />
-      </InputSearchContainer>
     </Container>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,92 @@
+import {
+  Container,
+  InputSearchContainer,
+  Header,
+  ListContainer,
+  Card,
+} from './styles';
+
+import Arrow from '../../assets/images/icons/arrow.svg';
+import Edit from '../../assets/images/icons/edit.svg';
+import Trash from '../../assets/images/icons/trash.svg';
+
 function Home() {
-  return 'Homepage';
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contato..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo Contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button" className="sort-button">
+            <span>Nome</span>
+            <img src={Arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>William Roger</strong>
+              <small>instagram</small>
+            </div>
+            <span>william@email.com</span>
+            <span>(81) 99999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={Edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={Trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>William Roger</strong>
+              <small>instagram</small>
+            </div>
+            <span>william@email.com</span>
+            <span>(81) 99999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={Edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={Trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>William Roger</strong>
+              <small>instagram</small>
+            </div>
+            <span>william@email.com</span>
+            <span>(81) 99999-9999</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={Edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={Trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }
 
 export default Home;
