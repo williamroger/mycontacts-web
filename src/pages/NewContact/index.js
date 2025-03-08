629c9f8f36a0220fe774e518b16d058e8cec950e
William Roger <williamroger.frontend@gmail.com>
2025-03-07 23:28:09 -0300

    adicionado componente Button

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import Input from '../../components/Input';
 import Select from '../../components/Select';
 import PageHeader from '../../components/PageHeader';
+import Button from '../../components/Button';
 
 function NewContact() {
   return (
@@ -12,6 +13,14 @@ function NewContact() {
         <option value="123">Instagram</option>
         <option value="124">Twitter</option>
       </Select>
+
+      <Button type="button">
+        Salvar alterações
+      </Button>
+
+      <Button type="button" disabled>
+        Salvar alterações
+      </Button>
     </>
   );
 }
