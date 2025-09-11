0b4bd6f2cdf3a1396b66f573e0f82a79842a0ca2
William Roger <williamroger.frontend@gmail.com>
2025-09-10 21:59:55 -0300

    adicionadas variações do Toast

--- src/components/Toast/ToastMessage/styles.js
+++ src/components/Toast/ToastMessage/styles.js
@@ -1,15 +1,27 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const containerVariants = {
+  default: css`
+    background-color: ${({ theme, type }) => theme.colors.primary.main};
+  `,
+  danger: css`
+    background-color: ${({ theme }) => theme.colors.danger.main};
+  `,
+  success: css`
+    background-color: ${({ theme }) => theme.colors.success.main};
+  `,
+};
 
 export const Container = styled.div`
   padding: 16px 32px;
-  background-color: ${({ theme, type }) => theme.colors.primary.main};
   color: #fff;
   border-radius: 4px;
   box-shadow: 0 20px 20px -16px rgba(0, 0, 0, 0.25);
-
   display: flex;
   align-items: center;
   justify-content: center;
+
+  ${({ type }) => containerVariants[type] || containerVariants.default}
 
   & + & {
     margin-top: 12px;
