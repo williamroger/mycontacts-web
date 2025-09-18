aa565200499c9e50fd01d6549948b8b5df3047a2
William Roger <williamroger.frontend@gmail.com>
2025-09-18 00:05:22 -0300

    adicionado método de remoção de listeners

--- src/lib/EventManager.js
+++ src/lib/EventManager.js
@@ -20,16 +20,44 @@ export default class EventManager {
       listener(payload);
     });
   }
+
+  removeListener(event, listenerToRemove) {
+    const listeners = this.listeners[event];
+    if (!listeners) {
+      return;
+    }
+
+    const filteredListeners = listeners.filter(
+      (listener) => listener !== listenerToRemove
+    );
+
+    this.listeners[event] = filteredListeners;
+  }
 }
 
 // Código provisório para testar o EventManager
 const toastEventManager = new EventManager();
 
-toastEventManager.on('addtoast', (payload) => {
-  console.log('addtoast listener 1:', payload);
-});
-toastEventManager.on('addtoast', (payload) => {
-  console.log('addtoast listener 2:', payload);
+function addToast1(payload) {
+  console.log('addToast1 ', payload);
+}
+
+function addToast2(payload) {
+  console.log('addToast2 ', payload);
+}
+
+toastEventManager.on('addtoast', addToast1);
+
+toastEventManager.on('addtoast', addToast2);
+
+toastEventManager.emit('addtoast', {
+  type: 'success',
+  text: 'Hello World!',
 });
 
-toastEventManager.emit('addtoast', { type: 'success', text: 'Hello World!' });
+toastEventManager.removeListener('addtoast', addToast1);
+
+toastEventManager.emit('addtoast', {
+  type: 'danger',
+  text: 'Hello World! Again!',
+});
