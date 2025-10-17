037cca542b4e834c820863dbe5ee0a53447d71b7
William Roger <williamroger.frontend@gmail.com>
2025-10-17 01:51:03 -0300

    adicionado método resetFields

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,11 @@
+import { useRef } from 'react';
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
 import ContactsService from '../../services/ContactsService';
 import toast from '../../utils/toast';
 
 function NewContact() {
+  const contactFormRef = useRef(null);
   async function handleSubmit(formData) {
     try {
       const contact = {
@@ -14,6 +16,7 @@ function NewContact() {
       };
 
       await ContactsService.createContact(contact);
+      contactFormRef.current.resetFields();
       toast({
         type: 'success',
         text: 'Contato cadastrado com sucesso!',
@@ -29,7 +32,11 @@ function NewContact() {
   return (
     <>
       <PageHeader title="Novo contato" />
-      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} />
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel="Cadastrar"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
